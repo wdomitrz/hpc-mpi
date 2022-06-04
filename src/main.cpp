458a69cc6343053c212c5823690946703a3a5fc6
Witalis Domitrz <witekdomitrz@gmail.com>
2022-06-04 14:13:09 +0200

    Get rank and number of processes

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,16 @@
 using std::cerr;
 
 int main(int argc, char* argv[]) {
+    int number_of_processes;
+    int myRank;
     auto [n, m, genome_in, queries_in, queries_out] = parse_args(argc, argv);
 
     MPI_Init(&argc, &argv); /* intialize the library with parameters caught by
                                the runtime */
+
+    MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
     MPI_Finalize();
     return 0;
 }
