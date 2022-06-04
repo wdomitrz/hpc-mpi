8f39e3db632aee4cb23f1a98b69457d6f88184a8
Witalis Domitrz <witekdomitrz@gmail.com>
2022-06-04 14:57:59 +0200

    Reading data

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,11 @@ int main(int argc, char* argv[]) {
     int my_rank;
     auto [n, m, genome_in, queries_in, queries_out] = parse_args(argc, argv);
 
-    MPI_Init(&argc, &argv); /* intialize the library with parameters caught by
-                               the runtime */
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    DataSource ds(genome_in);
-    for (int i = 0; i < n; i++) {
-        auto x = ds.getTotalGenomeSize(i), y = ds.getNodeGenomeSize(i),
-             z = ds.getNodeGenomeOffset(i);
-        for (int j = 0; j < number_of_processes; ++j) {
-            MPI_Barrier(MPI_COMM_WORLD);
-            if (j == my_rank) {
-                cerr << my_rank << "/" << number_of_processes << "\t" << i
-                     << "\t" << x << "\t" << y << "\t" << z << "\n";
-            }
-        }
-    }
+    DataSource data_source(genome_in);
 
     MPI_Finalize();
     return 0;
